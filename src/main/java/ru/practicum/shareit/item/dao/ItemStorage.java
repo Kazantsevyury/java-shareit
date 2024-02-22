8284efa5d89f037e3f5d1f25828dbebfda3b7d81
Kazantsev <ka.yu.8@yandex.ru>
2024-02-22 16:08:29 +0100

    checkstyle

--- src/main/java/ru/practicum/shareit/item/dao/ItemStorage.java
+++ src/main/java/ru/practicum/shareit/item/dao/ItemStorage.java
@@ -5,8 +5,10 @@ import ru.practicum.shareit.item.model.Item;
 
 import java.util.Collection;
 
+@SuppressWarnings("checkstyle:Regexp")
 public interface ItemStorage extends Dao<Item> {
     Collection<Item> findAllByOwnerId(Long ownerId);
+
     Collection<Item> searchAvailableItemsByText(String text);
 
 }
