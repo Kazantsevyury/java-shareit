0d22104c2fb234432edef4ec532a838bb06b7a30
Kazantsev <ka.yu.8@yandex.ru>
2024-02-22 14:11:01 +0100

    checkstyle

--- src/main/java/ru/practicum/shareit/request/service/ItemRequestService.java
+++ src/main/java/ru/practicum/shareit/request/service/ItemRequestService.java
@@ -4,9 +4,14 @@ import ru.practicum.shareit.request.dto.ItemRequestDto;
 import java.util.Collection;
 
 public interface ItemRequestService {
+
     ItemRequestDto addItemRequest(ItemRequestDto itemRequestDto);
+
     ItemRequestDto updateItemRequest(ItemRequestDto itemRequestDto);
+
     Collection<ItemRequestDto> getAllItemRequests();
+
     ItemRequestDto getItemRequestById(long itemRequestId);
+
     void removeItemRequest(long itemRequestId);
 }
