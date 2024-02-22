8b1eb65b7f16378146c35772d02a683edc849efa
Kazantsev <ka.yu.8@yandex.ru>
2024-02-22 05:16:35 +0100

    checkstyle

--- src/main/java/ru/practicum/shareit/request/service/impl/ItemRequestServiceImpl.java
+++ src/main/java/ru/practicum/shareit/request/service/impl/ItemRequestServiceImpl.java
@@ -5,14 +5,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import ru.practicum.shareit.booking.dao.BookingStorage;
 import ru.practicum.shareit.item.dao.ItemStorage;
-import ru.practicum.shareit.item.service.ItemService;
 import ru.practicum.shareit.request.ItemRequest;
 import ru.practicum.shareit.request.ItemRequestMapper;
 import ru.practicum.shareit.request.dao.ItemRequestStorage;
 import ru.practicum.shareit.request.dto.ItemRequestDto;
 import ru.practicum.shareit.request.service.ItemRequestService;
 import ru.practicum.shareit.user.dao.UserStorage;
-import ru.practicum.shareit.user.model.User;
 
 import java.util.ArrayList;
 import java.util.Collection;
@@ -29,37 +27,39 @@ public class ItemRequestServiceImpl implements ItemRequestService {
     private final ItemRequestMapper itemRequestMapper;
 
     @Override
-    public ItemRequestDto addItemRequest(ItemRequestDto itemRequestDto){
+    public ItemRequestDto addItemRequest(ItemRequestDto itemRequestDto) {
         final ItemRequest itemRequest = itemRequestMapper.dtoToItemRequest(itemRequestDto);
         final ItemRequest addedItemRequest = itemRequestStorage.add(itemRequest);
         log.info("Добавление нового ItemRequest: {}", addedItemRequest);
         return itemRequestMapper.itemRequestToDto(itemRequestStorage.findById(addedItemRequest.getId()));
     }
+
     @Override
-    public ItemRequestDto updateItemRequest(ItemRequestDto updatedItemRequestDto){
+    public ItemRequestDto updateItemRequest(ItemRequestDto updatedItemRequestDto) {
         final ItemRequest updatedItemRequest = itemRequestMapper.dtoToItemRequest(updatedItemRequestDto);
         final long itemId = updatedItemRequest.getId();
         itemRequestStorage.update(updatedItemRequest);
         log.info("Обновление ItemRequest с id {}: {}", itemId, updatedItemRequest);
         return itemRequestMapper.itemRequestToDto(itemRequestStorage.findById(itemId));
-
-
     }
+
     @Override
-    public Collection<ItemRequestDto> getAllItemRequests(){
+    public Collection<ItemRequestDto> getAllItemRequests() {
         log.info("Получение списка всех ItemRequests.");
         return itemRequestStorage.findAll().stream()
-                .map(itemRequestMapper :: itemRequestToDto)
+                .map(itemRequestMapper::itemRequestToDto)
                 .collect(Collectors.toCollection(ArrayList::new));
     }
+
     @Override
-    public ItemRequestDto getItemRequestById(long itemRequestId){
+    public ItemRequestDto getItemRequestById(long itemRequestId) {
         final ItemRequest itemRequest = itemRequestStorage.findById(itemRequestId);
         log.info("ItemRequest с id {} найден.", itemRequestId);
         return itemRequestMapper.itemRequestToDto(itemRequest);
     }
+
     @Override
-    public void removeItemRequest(long itemRequestId){
+    public void removeItemRequest(long itemRequestId) {
         itemRequestStorage.remove(itemRequestId);
         log.info("ItemRequest с id {} удалено.", itemRequestId);
     }
