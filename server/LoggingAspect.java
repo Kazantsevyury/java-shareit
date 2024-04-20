e65286316941cac5a0df216e247c738c7a2c52d5
Kazantsev <ka.yu.8@yandex.ru>
2024-04-20 16:48:48 +0200

    add pom.xml , LogginAspect

---
+++ server/LoggingAspect.java
@@ -0,0 +1,2 @@
+public class LoggingAspect {
+}
--- server/src/main/java/ru/practicum/shareit/aspect/LoggingAspect.java
+++ server/src/main/java/ru/practicum/shareit/aspect/LoggingAspect.java
@@ -1,4 +1,4 @@
-package ru.practicum.shareit;
+package ru.practicum.shareit.aspect;
 
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
